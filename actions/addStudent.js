cd8465104a1a0562aba5fb44ca66f2e3147a8076
NuminousBlue <jdiehl2236@gmail.com>
2023-01-06 00:12:30 -0600

    separated parsing concerns from io

--- actions/addStudent.js
+++ actions/addStudent.js
@@ -9,7 +9,9 @@ import { addStudentPrompt } from "../ui/index.js";
 export const addStudent = (fileHandler) => async () => {
   const { readFlowJson, paths, writeToTemp } = fileHandler();
   const { studentsWritePath } = paths;
+
   const { name, avg, groupNum } = await addStudentPrompt();
+
   const newStudent = Student(name, avg, parseInt(groupNum));
   const studentsJson = JSON.parse(readFlowJson(studentsWritePath));
   writeToTemp(studentsWritePath, [...studentsJson, newStudent]);
--- actions/import.js
+++ actions/import.js
@@ -12,27 +12,26 @@ import { Student } from "../lib/models.js";
  */
 
 export const importHandler = (fileHandler) => async (input) => {
-  const { readFlowJson, parser, writeToTemp, asyncTryCatch, paths } =
-    fileHandler(input);
+  const { readFlowJson, parser, writeToTemp, paths } = fileHandler(input);
 
   const { ext, localAbsolute, studentsWritePath } = paths;
 
   if (ext === ".csv") {
-    const jsonArray = await asyncTryCatch(
-      parser("formattedCsv")(localAbsolute)
-    )(localAbsolute);
+    const jsonArray = await parser("formattedCsv")(localAbsolute);
 
     const students = jsonArray.map((student) =>
       Student(student.name, student.avg, student.group)
     );
+
     writeToTemp(studentsWritePath, students);
     console.table(jsonArray);
     console.log(
       `The above data was successfully written to ${studentsWritePath}`
     );
   }
+
   if (ext === ".json") {
     const jsonArray = readFlowJson(localAbsolute);
-    writeToTemp(temp, jsonArray);
+    writeToTemp(studentsWritePath, jsonArray);
   }
 };
