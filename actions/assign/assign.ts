ee4f00569ed8b2776f1c4947f3d3b638d35a9e85
NuminousBlue <jdiehl2236@gmail.com>
2023-03-30 05:27:19 -0500

    refactor balance function

--- actions/assign/assign.ts
+++ actions/assign/assign.ts
@@ -27,19 +27,12 @@ export const utils: UtilsObject = {
   cleanRecords: (records) => records.filter((rec: StudentType) => rec.avg !== 0)
 };
 
-export const balance = (
-  records: RecArray,
-  columnName: string,
-  numGroups: number,
-  remainder: number,
+export function balance(
+  groups: RecArray,
   targetSD = 1,
-  iterations = 0
-): StudentType[] => {
-  const sorted = utils.sortDesc([...records], columnName);
-  const [outliers, pruned] = partition([...sorted], remainder);
-
-  const groupsMap = setupGroupsObject(numGroups, "array");
-  const groups = assign(1, pruned, groupsMap, numGroups);
+  iterations = 0,
+  outliers: any[]
+): StudentType[] {
   const groupAvgs = calcGroupAvgs(groups);
 
   const targetGroups = findTargetGroup(groupAvgs, outliers.length, []);
@@ -48,78 +41,78 @@ export const balance = (
 
   const avgs = calcGroupAvgs(preFinal);
 
-  const workerPool = new BalancerPool(4);
-  workerPool.executeJob({});
-
+  console.log(avgs);
   const SD = utils.standardDeviation(Object.values(avgs));
+  console.log(SD);
 
   if (SD > targetSD) {
-    if (iterations < 500) {
+    if (iterations < 2000) {
       return balance(
-        records,
-        columnName,
-        numGroups,
-        remainder,
+        groups.sort(() => Math.random() - 0.5),
         targetSD,
-        iterations + 1
+        iterations + 1,
+        outliers
       );
     } else {
       iterations = 0;
       return balance(
-        records,
-        columnName,
-        numGroups,
-        remainder,
+        groups.sort(() => Math.random() - 0.5),
         targetSD + 1,
-        iterations
+        iterations,
+        outliers
       );
     }
   }
   console.log(`Standard Deviation: ${SD.toFixed(2)}`);
   return preFinal;
-};
+}
 
-export const partition = (sorted: StudentType[], remainder: number) => {
+export function partition(sorted: StudentType[], remainder: number) {
   const outliers = popOutliers(sorted, remainder);
   const pruned = sorted.filter((student: any) =>
     student in outliers ? false : true
   );
 
   return [outliers, pruned];
-};
+}
 
-export const processRecords = (
+export function processRecords(
   records: RecArray,
   columnName: string,
   groupSize: number
-) => {
+) {
   const numStudents = records.length;
   const numGroups = floor(numStudents / groupSize);
   const remainder = numStudents % groupSize;
 
+  const sorted = utils.sortDesc([...records], columnName);
+  const [outliers, pruned] = partition([...sorted], remainder);
+
+  const groupsMap = setupGroupsObject(numGroups, "array");
+  const groups = assign(1, pruned, groupsMap, numGroups);
   // RECURSE
-  const finalized = balance(records, columnName, numGroups, remainder);
+  const finalized = balance(groups, numGroups, remainder, outliers);
 
   return finalized;
-};
+}
 
-export const assignOutliers = (
+export function assignOutliers(
   groups: any[],
   outliers: any,
   targets: string[]
-) => {
+) {
   targets.forEach((groupNum: any, idx: number) => {
     outliers[idx].group = groupNum;
     groups[groupNum]?.push(outliers[idx]);
   });
   return groups;
-};
+}
 
-export const findTargetGroup = (
+export function findTargetGroup(
   avgs: GroupsObject,
   numOutliers: number,
   targets: string[]
-): string[] => {
+): string[] {
   if (targets.length === numOutliers) return targets;
   const high = Object.values(avgs).reduce((a, b) => (a > b ? a : b));
 
@@ -133,7 +126,7 @@ export const findTargetGroup = (
   }
   // RECURSE
   return findTargetGroup(avgs, numOutliers, targets);
-};
+}
 
 export const popOutliers = (sorted: StudentType[], remainder: number) => {
   const outliers = [];
@@ -172,12 +165,12 @@ export const setupGroupsObject = (
   return groupsNumbered;
 };
 
-export const assign = (
+export function assign(
   current: number,
   studentRecords: StudentType[] | any,
   groupsMap: GroupsObject | any,
   numGroups: number
-): StudentType[] => {
+): StudentType[] {
   if (!studentRecords?.length) return groupsMap;
   let currentGroup = current;
 
@@ -195,7 +188,7 @@ export const assign = (
 
   // RECURSE
   return assign(currentGroup, filteredRecords, groupsMap, numGroups);
-};
+}
 
 const useDelivery = async (data: StudentType[]) => {
   const spinner = ora("Processing").start();
